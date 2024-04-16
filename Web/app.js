1245c4b1929871ec9df94aff84a607754954dcbe
saruhj <96214729+saruhj@users.noreply.github.com>
2024-04-16 17:42:38 -0400

    Added all button functionality

--- Web/app.js
+++ Web/app.js
@@ -1,5 +1,22 @@
 const logoutButton = document.getElementById('logoutButton');
 const displayBooks = document.getElementById('displayBooks');
+const addBooks = document.getElementById('addBooks');
+const searchBooks = document.getElementById('searchBooks');
+const removeBooks = document.getElementById('removeBooks');
+
+
+removeBooks.addEventListener('click', () => {
+	window.location.href = 'removeBooks.html';
+});
+
+addBooks.addEventListener('click', () => {
+	window.location.href = 'addBook.html';
+});
+
+searchBooks.addEventListener('click', () => {
+	window.location.href = 'searchBooks.html';
+});
+
 displayBooks.addEventListener('click', () => {
 	window.location.href = 'book-info.html';
 });
@@ -7,7 +24,6 @@ displayBooks.addEventListener('click', () => {
 logoutButton.addEventListener('click', () => {
 	window.location.href = 'index.html';
 });
-
 
 
 
