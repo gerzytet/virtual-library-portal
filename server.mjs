350ba009d0530573338c2d7702f6177c0dc615dc
Craig White <gerzytet@gmail.com>
2024-04-16 13:15:35 -0400

    Update server to host the web pages

--- server.mjs
+++ server.mjs
@@ -1,13 +1,14 @@
-import { createServer } from 'node:http';
+import express from 'express'
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+var server = express();
+//server.get('/', (req, res) => {
+//  res.send('Hello World');
+//});
+
+server.use(express.static('Web'));
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
