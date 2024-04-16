09289e763cda5d768e41698e7495e12b92af1af5
Craig White <gerzytet@gmail.com>
2024-04-16 13:45:46 -0400

    Link together login page with homepage

--- server.mjs
+++ server.mjs
@@ -1,4 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -8,6 +14,24 @@ var server = express();
 //  res.send('Hello World');
 //});
 
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.use('/', (req, res, next) => {
+  console.log('Request Type:', req.method)
+  console.log("body: ", req.body)
+  console.log("path: ", req.path)
+  next()
+})
+
+server.post('/index.html', (req, res, next) => {
+  if (req.body.username !== undefined && req.body.password !== undefined) {
+    console.log('Username: ' + req.body.username);
+    console.log('Password: ' + req.body.password);
+    res.sendFile(__dirname + '/Web/homepage.html');
+  } else {
+    next()
+  }
+})
 server.use(express.static('Web'));
 
 server.listen(port, hostname, () => {
