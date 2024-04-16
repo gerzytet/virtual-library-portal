b1b84e3cf0573c729eb553fb9154b7de58d9ad2c
saruhj <96214729+saruhj@users.noreply.github.com>
2024-04-15 20:27:32 -0400

    Update app.js
    Updated app.js to direct to login tab when logout button is hit

--- Web/app.js
+++ Web/app.js
@@ -1,7 +1,14 @@
-// Add a click event listener
+const logoutButton = document.getElementById('logoutButton');
+const displayBooks = document.getElementById('displayBooks');
 displayBooks.addEventListener('click', () => {
 	window.location.href = 'book-info.html';
 });
+
+logoutButton.addEventListener('click', () => {
+	window.location.href = 'login.html';
+});
+
+
 
 
 
