389d2379fed780182abe54b02cf8d6986b0bf792
Craig White <gerzytet@gmail.com>
2024-04-16 20:17:38 -0400

    Add login failure, and the data interface file

---
+++ data_interface.mjs
@@ -0,0 +1,34 @@
+export class Book {
+    constructor(title, author, publisher, year, isbn, category) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.isbn = isbn;
+        this.category = category;
+    }
+  }
+
+// Create an array of books
+const defaultBooks = [
+    new Book('JavaScript: The Good Parts', 'Douglas Crockford', 'O\'Reilly Media', 2008, '978-0596517748', 'Programming'),
+    new Book('Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin', 'Prentice Hall', 2008, '978-0132350884', 'Programming'),
+    new Book('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 'Pan Books', 1979, '978-0330258647', 'Science Fiction'),
+    // Add more books as needed
+];
+
+export class User {
+    getBookCollection() {
+        return defaultBooks;
+    }
+}
+
+//return a User object for the given username
+export function getUser(username) {
+    return new User();
+}
+
+//return true if the given username and password are valid credentials
+export function checkCredentials(username, password) {
+    return username === 'username' && password === 'password';
+}
--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import ejs from 'ejs';
+import { User, Book, checkCredentials, getUser } from './data_interface.mjs'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,43 +23,32 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 server.use('/', (req, res, next) => {
   console.log('Request Type:', req.method)
-  console.log("body: ", req.body)
+  //console.log("body: ", req.body)
   console.log("path: ", req.path)
   next()
 })
 
-class Book {
-  constructor(title, author, publisher, year, isbn, category) {
-      this.title = title;
-      this.author = author;
-      this.publisher = publisher;
-      this.year = year;
-      this.isbn = isbn;
-      this.category = category;
-  }
-}
-
-// Create an array of books
-const books = [
-  new Book('JavaScript: The Good Parts', 'Douglas Crockford', 'O\'Reilly Media', 2008, '978-0596517748', 'Programming'),
-  new Book('Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin', 'Prentice Hall', 2008, '978-0132350884', 'Programming'),
-  new Book('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 'Pan Books', 1979, '978-0330258647', 'Science Fiction'),
-  // Add more books as needed
-];
-
-books.push(books[0])
+//books.push(books[0])
 
 server.get('/book-info.html', (req, res, next) => {
   res.render(__dirname + '/Web/book-info.html', {books: books});
 })
 
+server.get('/', (req, res, next) => {
+  res.render(__dirname + '/Web/index.html', {login_failed: false});
+})
+
+server.get('/index.html', (req, res, next) => {
+  res.render(__dirname + '/Web/index.html', {login_failed: false});
+})
+
 server.post('/index.html', (req, res, next) => {
-  if (req.body.username !== undefined && req.body.password !== undefined) {
+  if (checkCredentials(req.body.username, req.body.password)) {
     console.log('Username: ' + req.body.username);
     console.log('Password: ' + req.body.password);
     res.sendFile(__dirname + '/Web/homepage.html');
   } else {
-    next()
+    res.render(__dirname + '/Web/index.html', {login_failed: true});
   }
 })
 server.use(express.static('Web'));
